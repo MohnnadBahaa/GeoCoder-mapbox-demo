f17b8556e606049e79988c228e3962b52ddca3d3
Mohannad Almusa <mohbah026@gmail.com>
2020-01-02 15:31:10 +0200

    clean up

--- server/app.js
+++ server/app.js
@@ -4,11 +4,17 @@ const env = require('dotenv');
 const cors = require('cors');
 const db = require('../database/config');
 
+// environment configuration
 env.config({ path: './config/config.env' });
 
+// database connection
 db();
+
 const app = express();
+// Body parsing middleware
 app.use(express.json());
+
+// enable node cors middleware
 app.use(cors());
 
 const PORT = process.env.PORT || 4000;
